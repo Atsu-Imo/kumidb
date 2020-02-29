9054b110bf4a47df88113a9b37adff4fa1b54eb0
Atsu-Imo <runo1go@gmail.com>
2020-02-29 23:04:10 +0900

    add shutdown control

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # kumidb
 
 ※参考にしました
-[Go言語でTCPのEchoサーバーを実直に実装する](https://qiita.com/kawasin73/items/3371d35166af733c2ce4)
+[GoでTCPサーバー上で独自プロトコルKey-Value Storeを実装する(知識編)](https://qiita.com/kawasin73/items/f28b4335715a0f474ac9)
+
+
+## その他参考にさせていただいた記事など
+
+[golang contextの使い方とか概念(contextとは)的な話](https://qiita.com/marnie_ms4/items/985d67c4c1b29e11fffc)
--- kumidb.go
+++ kumidb.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
 	"github.com/tidwall/buntdb"
 )
 
-func handleListener(listener *net.TCPListener) error {
-	defer listener.Close()
+const (
+	listenerCloseMatcher = "use of closed network connection"
+)
+
+// handlerListener
+// handlerConnection コネクション内部での処理
+func handleListener(serverCtx context.Context, listener *net.TCPListener, wg *sync.WaitGroup, chClosed chan struct{}) {
+	defer func() {
+		listener.Close()
+		close(chClosed)
+	}()
 	for {
 		conn, err := listener.AcceptTCP()
 		if err != nil {
@@ -17,31 +32,72 @@ func handleListener(listener *net.TCPListener) error {
 					log.Fatal("AcceptTCP", netError)
 				}
 			}
-			return err
+			//
+			if listenerCloseError(err) {
+				select {
+				// Contextがキャンセルされたときに閉じられるチャネルを返す
+				case <-serverCtx.Done():
+					return
+				default:
+					// 何もしない
+				}
+			}
 		}
-		go handlerConnection(conn)
+		// コネクションが1つできた
+		wg.Add(1)
+		go handlerConnection(serverCtx, conn, wg)
 	}
 }
-func handlerConnection(conn *net.TCPConn) error {
-	defer conn.Close()
-	buf := make([]byte, 1024)
+
+// handlerConnection
+func handlerConnection(serverCtx context.Context, conn *net.TCPConn, wg *sync.WaitGroup) {
+	defer func() {
+		conn.Close()
+		wg.Done()
+	}()
+
+	readCtx, errRead := context.WithCancel(context.Background())
+	go handleRead(conn, errRead)
+
+	select {
+	case <-readCtx.Done():
+	case <-serverCtx.Done():
+	}
+}
+
+func handleRead(conn *net.TCPConn, errRead context.CancelFunc) {
+	defer errRead()
+
+	buf := make([]byte, 4*1024)
+
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
-			if netError, ok := err.(net.Error); ok {
-				if netError.Temporary() {
-					log.Fatal("Read", netError)
+			if ne, ok := err.(net.Error); ok {
+				switch {
+				case ne.Temporary():
 					continue
 				}
 			}
-			return err
+			log.Println("Read", err)
+			return
 		}
+
 		n, err = conn.Write(buf[:n])
 		if err != nil {
-			log.Fatal("Write", err)
+			log.Println("Write", err)
+			return
 		}
 	}
 }
+
+func listenerCloseError(err error) bool {
+	return strings.Contains(err.Error(), listenerCloseMatcher)
+}
+
+// main
+// main シグナルを監視する
+// handlerListener tcpをacceptする. 1つ来るとhandleConnectionのgoroutineが新しくできる
 func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:6379")
 	if err != nil {
@@ -52,11 +108,43 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenTCP", err)
 	}
-	err = handleListener(listener)
-	if err != nil {
-		log.Fatal("handleListener", err)
+
+	signalChan := make(chan os.Signal, 1)
+	// whitelist SIGINT
+	signal.Ignore()
+	signal.Notify(signalChan, syscall.SIGINT)
+
+	var wg sync.WaitGroup
+	chClosed := make(chan struct{})
+
+	// serverCtxはすべてのgoroutineが同じContext上で動いていることを示す
+	// goroutine間の値の伝播
+	// context.Background()が親のContext
+	// context.WithCancel(ctx)によって子Contextと子Contextをキャンセルする関数が返る
+	serverCtx, shutdown := context.WithCancel(context.Background())
+
+	go handleListener(serverCtx, listener, &wg, chClosed)
+
+	log.Println("tcp server start")
+
+	// signalChanに(シグナル)が来るまで待機する
+	s := <-signalChan
+	switch s {
+	case syscall.SIGINT:
+		log.Println("Server shutdown start")
+		// 子コンテキストにキャンセルを伝える
+		shutdown()
+		listener.Close()
+
+		// すべてのgoroutineが閉じられるまで待つ
+		wg.Wait()
+		<-chClosed
+		log.Println("Server shutdown end")
+	default:
+		panic("unexpected signal")
 	}
 }
+
 func sample() {
 	db, err := buntdb.Open(":memory:")
 	if err != nil {
